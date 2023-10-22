db7cd83520312d97b84a7090ca4cefdf7fb9fbed
Thibault PLET <thibault.plet@gmail.com>
2023-10-22 22:23:31 +0200

    Add SSD1306DisplayComponent

---
+++ SSD1306DisplayComponent.h
@@ -0,0 +1,36 @@
+//
+// Created by Thibault PLET on 22/10/2023.
+//
+
+#ifndef COM_LILICLOUD_ARDUINOPROPERTY_SSD1306DISPLAYCOMPONENT_H
+#define COM_LILICLOUD_ARDUINOPROPERTY_SSD1306DISPLAYCOMPONENT_H
+
+#include <DisplayComponent.h>
+#include <Adafruit_SSD1306.h>
+
+class SSD1306DisplayComponent {
+protected:
+    /**
+     * Screen
+     */
+    Adafruit_SSD1306 * screen;
+
+public:
+    /**
+     * Constructor
+     */
+    SSD1306DisplayComponent(Adafruit_SSD1306 * screen) : DisplayComponent()
+    {
+        this->screen = screen;
+    }
+
+    /**
+     * Draw component
+     */
+    void draw(bool display = false)
+    {
+        DisplayComponent::draw();
+    }
+};
+
+#endif //COM_LILICLOUD_ARDUINOPROPERTY_SSD1306DISPLAYCOMPONENT_H
